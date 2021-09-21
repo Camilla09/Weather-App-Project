faf68c79695e3a6aece1f6a1b15f9f33dd097b60
Camilla <camilla.simonin@googlemail.com>
2021-09-21 21:04:06 +0200

    Create js file

---
+++ javascript.js
@@ -0,0 +1,116 @@
+body {
+  margin: 20px;
+  font-family: "Architects Daughter", cursive;
+}
+.container {
+  border-radius: 20px;
+  padding: 30px 60px;
+  background: radial-gradient(
+    827px at 47.3% 48%,
+    rgb(255, 255, 255) 0%,
+    rgb(138, 192, 216) 90%
+  );
+  border: 2px solid rgb(188, 188, 190);
+}
+h1 {
+  color: rgb(107, 107, 255);
+}
+div.form {
+  color: rgb(107, 107, 255);
+  padding: 12px 0;
+}
+.form-control {
+  border-color: rgb(107, 107, 255);
+}
+.btn.btn-primary.mb-2 {
+  background-color: rgb(107, 107, 255);
+}
+
+.col-1 {
+  padding: 0;
+}
+
+.location {
+  text-align: center;
+  font-size: 60px;
+  color: rgb(107, 107, 255);
+
+  margin-top: 30px;
+  font-family: "Gloria Hallelujah", cursive;
+  font-family: "Gochi Hand", cursive;
+  font-family: "Rock Salt", cursive;
+}
+.day-time {
+  text-align: center;
+  font-size: 40px;
+  color: rgb(107, 107, 255);
+  margin-left: 51px;
+}
+
+div.weather-icon {
+  text-align: center;
+  font-size: 150px;
+  margin-left: 120px;
+}
+span.temperature {
+  text-align: center;
+  font-size: 80px;
+  color: rgb(107, 107, 255);
+}
+hr {
+  border: 1px solid rgb(107, 107, 255);
+}
+
+.row {
+  text-align: center;
+}
+.row.other-parameters {
+  color: orange;
+  margin: 10px 120px;
+  font-size: 25px;
+  font-weight: 900;
+}
+.parameters-values {
+  color: rgb(107, 107, 255);
+  margin: 10px 120px 60px;
+
+  font-size: 25px;
+}
+
+.next-days {
+  color: palevioletred;
+  font-size: 25px;
+  margin-top: 70px;
+  font-weight: 900;
+}
+
+.cloud {
+  margin-top: 25px;
+}
+.sunandrain {
+  margin-top: 20px;
+}
+.temperatures-next-days {
+  color: rgb(107, 107, 255);
+  font-size: 25px;
+}
+button {
+  background-color: rgb(107, 107, 255);
+  color: white;
+  border: none;
+  border-radius: 10px;
+  padding: 10px 20px;
+  display: inline-block;
+  margin: 20px auto;
+  text-align: center;
+  transition: all 0.3s ease-out;
+}
+button:hover {
+  cursor: pointer;
+  background-color: white;
+  color: rgb(107, 107, 255);
+  border: 0.5px solid rgb(107, 107, 255);
+}
+.inline-center {
+  text-align: center;
+}
