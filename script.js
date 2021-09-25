55ad9f285d01e5a7c1446de08c70b50a65122907
Camilla <camilla.simonin@googlemail.com>
2021-09-25 17:24:14 +0200

    Update alt text to icon

--- script.js
+++ script.js
@@ -42,6 +42,9 @@ function showCurrentData(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].main);
 }
 
 function search(cityValue) {
